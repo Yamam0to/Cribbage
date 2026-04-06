25cbbf546d5c1bf9860edf561886088947766acb
Ajay Yamamoto <atyamamoto226@gmail.com>
2026-04-05 19:53:53 -0600

    Initial Commit

---
+++ Cribbage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cribbage
+//
+//  Created by Ajay Yamamoto on 2026-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cribbage/CribbageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CribbageApp.swift
+//  Cribbage
+//
+//  Created by Ajay Yamamoto on 2026-04-05.
+//
+
+import SwiftUI
+
+@main
+struct CribbageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
